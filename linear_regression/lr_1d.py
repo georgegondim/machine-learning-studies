5eb99b53995a88554df60002893899e65abb2349
gondimribeiro <gondimribeiro@gmail.com>
2016-11-15 23:54:29 -0200

    1D Linear Regression Example

---
+++ linear_regression/lr_1d.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+data = pd.read_csv('data_1d.csv').as_matrix()
+X = data[:, 0]
+Y = data[:, 1]
+
+# Compute intermediates values
+Ymean = Y.mean()
+Xmean = X.mean()
+Xsum = X.sum()
+Xsquared = X.dot(X)
+XYsum = X.dot(Y) 
+denominator = Xsquared - Xmean*Xsum
+
+# Compute a and b from y = ax + b
+a = (XYsum - Ymean*Xsum) / denominator
+b = (Ymean*Xsquared - Xmean*XYsum) / denominator
+
+# Compute predicted values
+Ypred = a*X + b
+
+# Plot data and predicted line
+plt.scatter(X, Y, label='Data')
+lines = plt.plot(X, Ypred, label='Regression')
+plt.setp(lines, color='r')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.show()
